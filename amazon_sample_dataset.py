bdfc1314eee4b457007fd5bd26eda52f53cd22db
Eric van Schaik <Eric.vanSchaik@poldervalley.com>
2022-05-02 17:20:38 +0200

    Moved example dataset code to separate file

--- amazon_sample_dataset.py
+++ amazon_sample_dataset.py
@@ -1,15 +1,39 @@
 import pandas as pd
 from torch.utils.data import Dataset
+import gensim.downloader
+import numpy as np
+import torch
 
 class AmazonSampleDataset(Dataset):
     
-    def __init__(self) -> None:
+    def __init__(self, labels=False) -> None:
         super().__init__()
-        self.PATH_TO_DATASET = 'C:/Users/ERSCHAIK/Qsync/Werk/Capgemini/Thesis/Dataset/1429_1.csv'
-        pd.read_csv(self.PATH_TO_DATASET)
+        self.PATH_TO_DATASET = 'D:/Users/Eric_/Downloads/sample.json'
+        self.dataset = pd.read_json(self.PATH_TO_DATASET, lines=True)
+        print(self.dataset)
+        
+        self.glove_vectors = gensim.downloader.load('glove-twitter-25')
+
+        self.data = ['this text is used to train the recurrent neural network', 'this text is also used to train the recurrent neural network']
+
+        # Create a numpy array of word vectors
+        training_vectors = np.concatenate([np.array([self.word_to_vec(word) for word in training_text.split()]) for training_text in self.data])
+
+        # Create a numpy array with all zeros with the same dimension as the training vectors (makes concatenation easier)
+        zero_vector = np.array([[0]*25])
+
+        if labels:
+            self.data = torch.Tensor(np.concatenate((zero_vector, training_vectors)))
+        else:
+            self.data = torch.Tensor(np.concatenate((training_vectors, zero_vector)))
+            
 
     def __len__(self):
-        return 0
+        return len(self.data)
 
     def __getitem__(self, index):
-        return 0
+        return self.data[index]
+
+    def word_to_vec(self, word):
+        ##TODO check for missing words not in glove_vectors
+        return self.glove_vectors[word]
--- information_estimator.py
+++ information_estimator.py
@@ -1,49 +1,45 @@
-import numpy as np
-
 import torch
 import torch.nn as nn
 import torch.optim
 from torch.utils.data import DataLoader
+from amazon_sample_dataset import AmazonSampleDataset
+import numpy as np
 
-import gensim.downloader
+summary_texts =  ['we need the information amount of this text', 'we need the information amount of this text']
 
-training_text = 'this text is used to train the recurrent neural network'
-test_text =  'we need the information amount of this text'
+def train_rnn(data, labels):
+    # amazon_dataset = AmazonSampleDataset()
 
-glove_vectors = gensim.downloader.load('glove-twitter-25')
+    # Since the RNN needs to predict the next word, it is easiest to take as input a zero vector followed by all the word vectors, while the labels can be seen as the opposite
+    training_data = DataLoader(data, batch_size=5, shuffle=True)
+    training_labels = DataLoader(labels, batch_size=5, shuffle=True)
 
-def word_to_vec(word):
-    ##TODO check for missing words not in glove_vectors
-    return glove_vectors[word]
+    rnn = nn.RNN(25, 25, 3)
 
-# Create a numpy array of word vectors
-training_vectors = np.array([word_to_vec(word) for word in training_text.split()])
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.SGD(rnn.parameters(), lr=0.01)
 
-# Create a numpy array with all zeros with the same dimension as the training vectors (makes concatenation easier)
-zero_vector = np.array([[0]*25])
+    lss_avg = []
 
-# Since the RNN needs to predict the next word, it is easiest to take as input a zero vector followed by all the word vectors, while the labels can be seen as the opposite
-training_data = DataLoader(torch.Tensor(np.concatenate((zero_vector, training_vectors))), batch_size=5, shuffle=True)
-training_labels = DataLoader(torch.Tensor(np.concatenate((training_vectors, zero_vector))), batch_size=5, shuffle=True)
+    for _ in range(1, 10):
+        lss_per_epoch = []
+        for _ in range(1, 100):
+            lss_per_batch = []
+            for _, (batch_data, batch_labels) in enumerate(zip(training_data, training_labels)):
+                optimizer.zero_grad()
+                prob = rnn(batch_data)
+                loss = criterion(prob[0], batch_labels)
+                lss_per_batch += [loss.item()]
+                loss.backward()
+                optimizer.step()
+            lss_per_epoch += [sum(lss_per_batch) / len(lss_per_batch)]
+        lss_avg += [sum(lss_per_epoch) / len(lss_per_epoch)]
+    print(lss_avg)
 
-rnn = nn.RNN(25, 25, 1)
+    return rnn
 
-criterion = nn.MSELoss()
-optimizer = torch.optim.SGD(rnn.parameters(), lr=0.01)
+def test_rnn(rnn, summaries):
+    return 0
 
-lss_avg = []
-
-for epic_batch in range(1, 10):
-    lss_per_epoch = []
-    for epoch in range(1, 100):
-        lss_per_batch = []
-        for i, (batch_data, batch_labels) in enumerate(zip(training_data, training_labels)):
-            optimizer.zero_grad()
-            prob = rnn(batch_data)
-            loss = criterion(prob[0], batch_labels)
-            lss_per_batch += [loss.item()]
-            loss.backward()
-            optimizer.step()
-        lss_per_epoch += [sum(lss_per_batch) / len(lss_per_batch)]
-    lss_avg += [sum(lss_per_epoch) / len(lss_per_epoch)]
-print(lss_avg)
+train_rnn(AmazonSampleDataset(), AmazonSampleDataset(labels=True))
+# test_rnn(summary_texts)
