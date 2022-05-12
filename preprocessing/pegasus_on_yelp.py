7dbd8cd2d222a93ea8f8a322088424f9a0325f95
Eric van Schaik <schaikejgv@gmail.com>
2022-05-12 13:55:33 +0200

    Check for too short or too long sentences when performing pegasus summarization on yelp

--- preprocessing/pegasus_on_yelp.py
+++ preprocessing/pegasus_on_yelp.py
@@ -6,7 +6,12 @@ import os
 def summarize_yelp():
     yelp_df = df_read_json(os.path.join(os.getcwd(), 'my_datasets', 'yelp_text2.json'))
     pegasus = transformers.pipeline(task='summarization', model='google/pegasus-xsum')
-    yelp_df_sample = (yelp_df['text'][:100]).to_list()
+    yelp_df_sample = pd.DataFrame(yelp_df['text'][:100])
+    df_split = pd.DataFrame(yelp_df_sample['text'].apply(lambda x: x.split()))
+    df_split = df_split[df_split['text'].map(len) > 64]
+    df_split = df_split[df_split['text'].map(len) < 512]
+    yelp_df_sample = yelp_df_sample.filter(items=df_split.index, axis=0)
     pegasus_df = pd.DataFrame.from_dict(pegasus(inputs=yelp_df_sample))
     pegasus_df.rename(columns={'summary_text': 'text'}, inplace=True)
+    pegasus_df = pegasus_df[len(pegasus_df['text']) > 50]
     df_to_json(pegasus_df, os.path.join(os.getcwd(), 'my_datasets', 'pegasus_on_yelp_summaries.json'))
