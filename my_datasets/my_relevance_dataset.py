55c633ac789533ed177254eb07d3c08ac745a656
Eric van Schaik <schaikejgv@gmail.com>
2022-05-19 16:33:57 +0200

    Cleaned up dataset classes

--- my_datasets/my_relevance_dataset.py
+++
@@ -1,9 +0,0 @@
-from my_datasets.my_vectorized_dataset import MyVectorizedDataset
-
-
-class MyRelevanceDataset(MyVectorizedDataset):
-
-    def __init__(self, path_to_dataset) -> None:
-        super().__init__(path_to_dataset)
-
-        
