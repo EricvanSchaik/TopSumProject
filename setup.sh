db624899a914d60d3aecf20ad0f2ea7881fb4e58
Eric van Schaik <schaikejgv@gmail.com>
2022-07-19 13:54:47 +0200

    Replaced setup.sh with requirements.txt

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-conda create -n researchTopics python=3.8
-conda activate researchTopics
-conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch 
-conda install transformers datasets -c huggingface -c conda-forge
-conda install sentencepiece gensim jupyter bertopic
