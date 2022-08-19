666e344036d5f2847693558bc279b1df219a92df
Eric van Schaik <schaikejgv@gmail.com>
2022-08-19 09:34:48 +0200

    Fixed measure_summaries

--- src/metrics/measure_summaries.py
+++ src/metrics/measure_summaries.py
@@ -4,7 +4,7 @@ from src.metrics.word_frequency import count_words
 
 def measure_summaries(path: str) -> str:
     results = ''
-    # results += ('\t next word prediction: ' + str(test_summaries(path)) + '\n')
+    results += ('\t next word prediction: ' + str(test_summaries(path)) + '\n')
     results += ('\t average distance of word vector to topic: ' + str(calculate_relevance(path)) + '\n')
     results += ('\t aggregrated frequences of most frequent words: ' + count_words(path) + '\n')
     return results
