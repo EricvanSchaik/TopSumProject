Fixed relevance calculation (untested)

data/my_datasets/my_vectorized_dataset.py
@@ -1,44 +1,34 @@
 from torch.utils.data import Dataset
-from src.helpers.preprocess_text import clean_text
+from src.helpers.word_to_vec import WordToVec
 from src.helpers.serialization import df_read_json, df_to_json
 from os.path import exists
 import pandas as pd
-import gensim.downloader
-import numpy as np
-import torch
 
 class MyVectorizedDataset(Dataset):
 
-    def __init__(self, path_to_dataset: str) -> None:
+    def __init__(self, path_to_dataset: str, w2v: WordToVec) -> None:
         super().__init__()
         path_to_vectorized = path_to_dataset[:-5] + '_vectorized.json'
         self.review_vectors = []
         if (exists(path_to_vectorized)):
             self.review_vectors = df_read_json(path_to_vectorized).to_numpy()
         else:
-            self.dataset = pd.read_json(path_to_dataset, lines=True)
-            self.glove_vectors = gensim.downloader.load('glove-twitter-25')
-
-            for review in self.dataset['text'].tolist():
-                word_vectors = []
-                for word in review.split():
-                    if len(word_vectors) == 0:
-                        word_vectors = np.array([self.word_to_vec(word)])
+            self.dataset = df_read_json(path_to_dataset)
+            
+            for review in self.dataset['text']:
+                if isinstance(review, str):
+                    word_vectors = []
+                    for word in review.split():
+                        if len(word_vectors) == 0:
+                            word_vectors = [w2v.word_to_vec(word).tolist()]
+                        else:
+                            word_vectors += [w2v.word_to_vec(word).tolist()]
+                    if len(self.review_vectors) == 0:
+                        self.review_vectors = [word_vectors]
                     else:
-                        word_vectors = np.concatenate((word_vectors, np.array([self.word_to_vec(word)])))
-                if len(self.review_vectors) == 0:
-                    self.review_vectors = [word_vectors]
-                else:
-                    self.review_vectors.append([word_vectors])
-
+                        self.review_vectors += [word_vectors]
+            print(self.review_vectors)
             df_to_json(pd.DataFrame(self.review_vectors), path_to_dataset[:-5] + '_vectorized.json')
-
-    def word_to_vec(self, word):
-        word = clean_text(word)
-        if word in self.glove_vectors:
-            return self.glove_vectors[word]
-        else:
-            return np.array([0]*25)
 
     def __len__(self):
         return len(self.review_vectors)

src/helpers/word_to_vec.py
@@ -1,11 +1,18 @@
 import numpy as np
 import gensim.downloader
+import string
 
+class WordToVec():
 
-def word_to_vec(word):    
-    glove_vectors = gensim.downloader.load('glove-twitter-25')
-    ##TODO Remove stop tokens from word
-    if word.lower() in glove_vectors:
-        return glove_vectors[word.lower()]
-    else:
-        return np.array([0]*25)
+    def __init__(self) -> None:
+        self.glove_vectors = gensim.downloader.load('glove-twitter-25')
+
+    def clean_text(self, text: str) -> str:
+        return text.translate(str.maketrans('', '', string.punctuation)).lower()
+
+    def word_to_vec(self, word):
+        word = self.clean_text(word)
+        if word in self.glove_vectors:
+            return self.glove_vectors[word]
+        else:
+            return np.array([0]*25)

src/metrics/relevance_calculator.py
@@ -1,18 +1,23 @@
 from src.helpers.serialization import df_read_json
 from data.my_datasets.my_vectorized_dataset import MyVectorizedDataset
 import numpy as np
-from src.helpers.word_to_vec import word_to_vec
+from src.helpers.word_to_vec import WordToVec
 
 def calculate_relevance(path: str) -> np.float64:
-    dataset = MyVectorizedDataset(path)
+    print('start calculating relevance')
+    w2v = WordToVec()
+    dataset = MyVectorizedDataset(path, w2v)
+    print('vectorized dataset generated')
     df = df_read_json(path)
     subjects = df['product_category']
     distances = []
-
-    for index, review_vectors in enumerate(dataset):
-        subject = word_to_vec(subjects[index])
-        for word_vector in review_vectors:
-            distance = np.linalg.norm(subject - np.array(word_vector))
-            distances.append(distance)
+    
+    for review_index, review_vectors in enumerate(dataset):
+        subject = w2v.word_to_vec(subjects[review_index])
+        for word_index, word_vector in enumerate(review_vectors):
+            if word_vector is not None:
+                distance = np.linalg.norm(subject - np.array(word_vector))
+                distances.append(distance)
     avg = (sum(distances) / len(distances))
+    print(avg)
     return avg