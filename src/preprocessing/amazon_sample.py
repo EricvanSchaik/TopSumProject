626679926210a9872a41fb416598d3b9ab9218fd
Eric van Schaik <schaikejgv@gmail.com>
2022-08-25 17:06:54 +0200

    Create dataset with specific number of reviews per product

--- src/preprocessing/amazon_sample.py
+++ src/preprocessing/amazon_sample.py
@@ -1,9 +1,11 @@
+from operator import concat
 from re import sub
 from datasets import load_dataset
 import json
 import pandas as pd
 
-sample_size = 100000
+# sample_size = 100000
+sample_size = 10000
 
 def sample_amazon():
     amazon_dataset = load_dataset('amazon_us_reviews', 'Tools_v1_00', split='train')
@@ -15,6 +17,7 @@ def sample_amazon():
     jsonFile.write(json.dumps(review_texts_sample))
     jsonFile.close()
 
+
 def sort_amazon():
     amazon_dataset = load_dataset('amazon_us_reviews', 'Electronics_v1_00', split='train')
     all_column_names = list(amazon_dataset.features.keys())
@@ -22,7 +25,7 @@ def sort_amazon():
     review_texts = review_texts.sort('product_id')
     result = pd.DataFrame()
     freq_counts = pd.DataFrame()
-    for i in range((len(review_texts) // sample_size)):
+    for i in range(len(review_texts) // sample_size):
         part_df = pd.DataFrame(review_texts[i*sample_size:(i+1)*sample_size])
         freq_count = part_df.value_counts(subset=['product_id']).to_frame().reset_index()
         freq_counts = pd.concat([freq_counts, freq_count]).sort_values(by=0, ascending=False)
@@ -38,3 +41,19 @@ def sort_amazon():
         result = result[result['product_id'].isin(relevant_products)]
     result = result.reset_index()
     result.to_csv('./data/amazon_sorted/most_populair_products.csv', index=False)
+
+
+def get_n_reviews(n: int):
+    amazon_dataset = load_dataset('amazon_us_reviews', 'Electronics_v1_00', split='train')
+    all_column_names = list(amazon_dataset.features.keys())
+    useful_column_names = ['product_id', 'review_body', 'product_category']
+    amazon_dataset = amazon_dataset.remove_columns([c for c in all_column_names if c not in useful_column_names])
+    amazon_dataset = amazon_dataset.sort('product_id')
+    result = pd.DataFrame(columns=useful_column_names)
+    for index in range(len(amazon_dataset)):
+        product_ids = amazon_dataset[index:index+n]['product_id']
+        if len([x for x in product_ids if x == product_ids[0]]) == n and product_ids[0] not in result['product_id'].tolist():
+            result = pd.concat([result, pd.DataFrame.from_dict(amazon_dataset[index:index+n])])
+        if len(result) > sample_size:
+            break
+    result.to_csv('./data/amazon_sorted/products_' + str(n) + '_reviews.csv', index=False)
