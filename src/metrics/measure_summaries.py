e809a291b5dcb1919173a07f89d30deb50e67d67
Eric van Schaik <schaikejgv@gmail.com>
2022-08-30 13:07:32 +0200

    Removed ROUGE calculation (as you need reference summaries for that, which we don't have, I forgot)

--- src/metrics/measure_summaries.py
+++ src/metrics/measure_summaries.py
@@ -1,4 +1,3 @@
-from src.metrics.rouge_calculation import calculate_rouge
 from src.metrics.sentiment_deviation import measure_deviation
 from src.metrics.information_estimator import test_summaries
 from src.metrics.relevance_calculator import calculate_relevance
@@ -6,8 +5,6 @@ from src.metrics.word_frequency import count_words
 
 def measure_summaries(summaries_path: str, reviews_path: str) -> str:
     results = ''
-    print('rouge score calculation')
-    results += ('\t ROUGE score: ' + calculate_rouge(summaries_path, reviews_path) + '\n')
     print('next word prediction')
     results += ('\t next word prediction: ' + str(test_summaries(summaries_path)) + '\n')
     print('relevance calculation')
@@ -15,5 +12,5 @@ def measure_summaries(summaries_path: str, reviews_path: str) -> str:
     print('count words')
     results += ('\t aggregrated frequences of most frequent words: ' + count_words(summaries_path) + '\n')
     print('sentiment deviation calculation')
-    # results += ('\t sentiment deviation: ' + measure_deviation(summaries_path, reviews_path) + '\n')
+    results += ('\t sentiment deviation: ' + measure_deviation(summaries_path, reviews_path) + '\n')
     return results
--- src/metrics/rouge_calculation.py
+++
@@ -1,4 +0,0 @@
-
-
-def calculate_rouge(summaries_path: str, reviews_path: str) -> str:
-    return ''
