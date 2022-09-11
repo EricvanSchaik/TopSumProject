d708a01ecf68872039b52624fd715d418b654d37
Eric van Schaik <schaikejgv@gmail.com>
2022-09-11 18:59:47 +0200

    Run the whole model on the cluster

--- main.py
+++ main.py
@@ -25,41 +25,41 @@ if __name__ == '__main__':
 
     reviews_path = './data/sorted_' + dataset + '/products_8_reviews.json'
     
-    # topic_model_path = './results/' + dataset + '/topic_model_all_products'
+    topic_model_path = './results/' + dataset + '/topic_model_all_products'
 
-    # nr_topics = 10
+    nr_topics = 10
 
-    # topic_model, predictions = make_predictions(reviews_path, topic_model_path, nr_topics=nr_topics)
-    # print('topic model and predictions generated')
+    topic_model, predictions = make_predictions(reviews_path, topic_model_path, nr_topics=nr_topics)
+    print('topic model and predictions generated')
 
-    # rankings_path = './results/' + dataset + '/rankings'
-    # w2v = gensim.downloader.load('word2vec-google-news-300')
-    # rankings_per_product = rank_reviews(results_path=rankings_path, reviews_path=reviews_path, topic_model=topic_model, all_reviews_predictions=predictions[1], w2v=w2v)
-    # print('sentences ranked')
+    rankings_path = './results/' + dataset + '/rankings'
+    w2v = gensim.downloader.load('word2vec-google-news-300')
+    rankings_per_product = rank_reviews(results_path=rankings_path, reviews_path=reviews_path, topic_model=topic_model, all_reviews_predictions=predictions[1], w2v=w2v)
+    print('sentences ranked')
 
-    # topsum_path = './results/' + dataset + '/topsum_summaries.json'
-    # final_summaries = summarize(reviews_path, rankings_per_product=rankings_per_product, results_path=topsum_path)
-    # print('summaries generated')
+    topsum_path = './results/' + dataset + '/topsum_summaries.json'
+    final_summaries = summarize(reviews_path, rankings_per_product=rankings_per_product, results_path=topsum_path)
+    print('summaries generated')
 
-    # results = 'topsum measurements:\n'
-    # print('measuring topsum')
-    # results += measure_summaries(topsum_path, reviews_path)
+    results = 'topsum measurements:\n'
+    print('measuring topsum')
+    results += measure_summaries(topsum_path, reviews_path)
 
-    # results += '\n distilbart measurements:\n'
+    results += '\n distilbart measurements:\n'
 
     distilbart_path = './data/distilbart/distilbart_on_' + dataset + '_summaries.json'
     if not os.path.exists(distilbart_path):
         summarize_amazon(distilbart_path, reviews_path)
     print('measuring distilbart')
-    # results += measure_summaries(distilbart_path, reviews_path)
+    results += measure_summaries(distilbart_path, reviews_path)
 
-    # print('measuring coop')
-    # results += '\n coop measurements:\n'
-    # results += measure_summaries('./data/coop/coop_on_' + dataset + '_summaries.json', reviews_path)
-    # print(results)
-    # results_file = open('./results/' + dataset + '/measurements.txt', 'w')
-    # results_file.write(results)
-    # results_file.close()
+    print('measuring coop')
+    results += '\n coop measurements:\n'
+    results += measure_summaries('./data/coop/coop_on_' + dataset + '_summaries.json', reviews_path)
+    print(results)
+    results_file = open('./results/' + dataset + '/measurements.txt', 'w')
+    results_file.write(results)
+    results_file.close()
 
     end = time.time()
     print('this script took ' + str(end-start) + ' seconds')
--- src/preprocessing/distilbart_on_amazon.py
+++ src/preprocessing/distilbart_on_amazon.py
@@ -16,7 +16,7 @@ def summarize_amazon(distilbart_path: str, reviews_path: str):
         for review in product_reviews:
             full_text += '\n' + review
         review_summaries.append(summarizer(full_text)[0]['summary_text'])
-    results_df = pd.DataFrame.from_dict(summarizer(inputs=review_summaries, max_length=50))
+    results_df = pd.DataFrame.from_dict(summarizer(inputs=review_summaries, max_length=60))
     results_df.rename(columns={'summary_text': 'text'}, inplace=True)
     results_df['product_category'] = amazon_df['product_category'][0]
     results_df['product_id'] = product_ids
